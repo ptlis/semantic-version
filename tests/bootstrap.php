24ef0029d18c5e7f9b9d29f8934dcc26080eed4e
ptlis <ptlis@ptlis.net>
2014-02-15 17:20:57 +0000

    Undo autoloader silliness in phpunit bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,3 @@
 <?php
-if (file_exists(__DIR__ . '/vendor/autoload.php')) {
-    require_once __DIR__ . '/vendor/autoload.php';
-} elseif (file_exists(__DIR__ . '/../vendor/autoload.php')) {
-    require_once __DIR__ . '/../vendor/autoload.php';
-} elseif (file_exists(__DIR__ . '/../../../vendor/autoload.php')) {
-    require_once __DIR__ . '/../../../vendor/autoload.php';
-} else {
-    throw new RuntimeException('vendor/autoload.php could not be found.');
-}
+
+require_once __DIR__ . '/../vendor/autoload.php';
