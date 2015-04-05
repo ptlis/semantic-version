386594831ca6a62c7ea703b292ece23d90e87dab
ptlis <ptlis@ptlis.net>
2015-04-05 16:09:24 +0100

    Add version value type,

---
+++ src/Version/Version.php
@@ -0,0 +1,128 @@
+<?php
+
+/**
+ * PHP Version 5.3
+ *
+ * @copyright   (c) 2015 brian ridley
+ * @author      brian ridley <ptlis@ptlis.net>
+ * @license     http://opensource.org/licenses/MIT MIT
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ptlis\SemanticVersion\Version;
+
+use ptlis\SemanticVersion\Version\Label\Label;
+use ptlis\SemanticVersion\Version\Label\LabelInterface;
+
+/**
+ * Value type for simple version numbers.
+ */
+class Version implements VersionInterface
+{
+    /**
+     * @var string
+     */
+    private $major;
+
+    /**
+     * @var string
+     */
+    private $minor = 0;
+
+    /**
+     * @var string
+     */
+    private $patch = 0;
+
+    /**
+     * @var LabelInterface
+     */
+    private $label;
+
+
+    /**
+     * Constructor.
+     *
+     * If no label is passed then an absent label type will be created & used.
+     *
+     * @param int $major
+     * @param int $minor
+     * @param int $patch
+     * @param LabelInterface|null $label
+     */
+    public function __construct($major, $minor = 0, $patch = 0, LabelInterface $label = null)
+    {
+        if (is_null($label)) {
+            $label = new Label(Label::PRECEDENCE_ABSENT);
+        }
+
+        $this->major = $major;
+        $this->minor = $minor;
+        $this->patch = $patch;
+        $this->label = $label;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function isSatisfiedBy(VersionInterface $version)
+    {
+        return strval($this) === strval($version);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getMajor()
+    {
+        return $this->major;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getMinor()
+    {
+        return $this->minor;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getPatch()
+    {
+        return $this->patch;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getLabel()
+    {
+        return $this->label;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function __toString()
+    {
+        $strVersion = implode(
+            '.',
+            array(
+                $this->major,
+                $this->minor,
+                $this->patch
+            )
+        );
+
+        // Only include the label portion if not absent
+        if (Label::PRECEDENCE_ABSENT !== $this->label->getPrecedence()) {
+            $strVersion .= '-' . $this->label;
+        }
+
+        return $strVersion;
+    }
+}
---
+++ tests/Version/VersionTest.php
@@ -0,0 +1,122 @@
+<?php
+
+/**
+ * PHP Version 5.3
+ *
+ * @copyright   (c) 2015 brian ridley
+ * @author      brian ridley <ptlis@ptlis.net>
+ * @license     http://opensource.org/licenses/MIT MIT
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ptlis\SemanticVersion\Test\Version;
+
+use ptlis\SemanticVersion\Version\Label\Label;
+use ptlis\SemanticVersion\Version\Version;
+
+class LabelTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCreate()
+    {
+        $version = new Version(
+            1,
+            0,
+            5,
+            new Label(Label::PRECEDENCE_ALPHA, 'alpha', 1)
+        );
+
+        $this->assertEquals(
+            1,
+            $version->getMajor()
+        );
+
+        $this->assertEquals(
+            0,
+            $version->getMinor()
+        );
+
+        $this->assertEquals(
+            5,
+            $version->getPatch()
+        );
+
+        $this->assertEquals(
+            new Label(Label::PRECEDENCE_ALPHA, 'alpha', 1),
+            $version->getLabel()
+        );
+
+        $this->assertEquals(
+            '1.0.5-alpha.1',
+            strval($version)
+        );
+    }
+
+    public function testCreateWithDefaults()
+    {
+        $version = new Version(
+            1
+        );
+
+        $this->assertEquals(
+            1,
+            $version->getMajor()
+        );
+
+        $this->assertEquals(
+            0,
+            $version->getMinor()
+        );
+
+        $this->assertEquals(
+            0,
+            $version->getPatch()
+        );
+
+        $this->assertEquals(
+            new Label(Label::PRECEDENCE_ABSENT),
+            $version->getLabel()
+        );
+    }
+
+    public function testCreateAbsent()
+    {
+        $version = new Version(
+            1,
+            0,
+            5
+        );
+
+        $this->assertEquals(
+            '1.0.5',
+            strval($version)
+        );
+    }
+
+    public function testIsSatisfiedBy()
+    {
+        $version = new Version(
+            1,
+            0,
+            5
+        );
+
+        $this->assertTrue(
+            $version->isSatisfiedBy(new Version(1, 0, 5))
+        );
+    }
+
+    public function testIsNotSatisfiedBy()
+    {
+        $version = new Version(
+            1,
+            0,
+            5
+        );
+
+        $this->assertFalse(
+            $version->isSatisfiedBy(new Version(1, 0, 6))
+        );
+    }
+}
